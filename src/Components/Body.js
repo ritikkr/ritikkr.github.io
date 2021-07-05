fe912d3bd47d95f90f6840fd22bee28805fb91d3
Ritik Kumar <ritikkumar2405199@gmail.com>
2021-07-05 19:52:56 +0530

    Responsiveness added into module

---
+++ src/Components/Body.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { ReactComponent as Icon } from "./LandingImage.svg";
+import githubIcon from "./Icons/icons8-github.gif";
+import linkedinIcon from "./Icons/icons8-linkedin-circled.gif";
+import twitterIcon from "./Icons/icons8-twitter-circled.gif";
+import stackOverflowIcon from "./Icons/icons8-stack-overflow.svg";
+import mailIcon from "./Icons/icons8-secured-letter.gif";
+import "./Body.css";
+import "./CSS/homeSvg.css";
+const Body = () => {
+  return (
+    <div className="body-container">
+      <div className="about-left-container">
+        <span id="handGesture">👋</span>
+        <p id="myName">Hello, My Name is Ritik</p>
+        <p id="litIntro"> ➤ 👨‍💻 I'm Web and Software Developer from India</p>
+        <span id="flag">🇮🇳</span>
+        <br></br>
+        <a href="" id="Resume">
+          Resume
+        </a>
+        <div className="social-icons">
+          <ul>
+            <li>
+              <a href="">
+                <img src={githubIcon} alt="" height="45px" width="45x" />
+              </a>
+            </li>
+            
+            <li>
+              <a href="">
+                <img src={linkedinIcon} alt="" height="45px" width="45x" />
+              </a>
+            </li>
+            
+            <li>
+              <a href="">
+                <img src={twitterIcon} alt="" height="45px" width="45x" />
+              </a>
+            </li>
+            
+            <li>
+              <a href="">
+                <img src={stackOverflowIcon} alt="" height="45px" width="45x" />
+              </a>
+            </li>
+            
+            <li>
+              <a href="">
+                <img src={mailIcon} alt="" height="45px" width="45x" />
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+      <div className="svg-right-container">
+        <Icon className="svgImg" />
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./Nav.css";
+import menuIcon from "./Icons/icons8-menu.svg"
+const intro = () => {
+    let myIntro = ["Machine Learning", "Web Development", ""]
+}
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="header-container">
+        <div className="logo-div">
+          <p id="nameLogo">Ritik Kr.</p>
+        </div>
+        <div className="menu-div">
+          <ul id="menu-list">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <li>
+              <a href="">Resume</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  
+  );
+};
+
+export default Navbar;
