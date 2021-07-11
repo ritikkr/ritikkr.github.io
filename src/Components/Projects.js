327fef54c36ec9a128ab8e536cd2c4cbb2e61642
Ritik Kumar <ritikkumar2405199@gmail.com>
2021-07-11 18:21:07 +0530

    project page responsivess has been improved

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,68 +1,66 @@
-import React, { Component } from 'react';       
-import './CSS/Project.css'
-import portfolioImg from './static/projectImage/portfolio2.png'
+import React, { Component } from "react";
+import "./CSS/Project.css";
+import portfolioImg from "./static/projectImage/portfolio3.png";
+import covid19india from "./static/projectImage/covid.jpg";
+import rejango from "./static/projectImage/rejango.svg";
 class Projects extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div className="project-container">
-                <div className="project-header-container">
-                    <h3 className="project-head">My Projects in Shorts</h3>
-                </div>
-                <div className="project-cards-container">
-                    <div className="project-card">
-                        <div className="img-container">
-                        <img src={portfolioImg} alt="" className="project-card-img" />
-                        </div>
-                        <div className="description-container">
-                            <h2 class="project-card-head">Portfolio Website</h2>
-                            <p className="project-details">
-                                This is my portfolio website made with React Js using react-dom, react-router
-                            this is something that i have to solve
-                            </p>
-                        </div>
-                    </div>
-                    <div className="project-card">
-                        <div className="img-container">
-                        <img src={portfolioImg} alt="" className="project-card-img" />
-                        </div>
-                        <div className="description-container">
-                            <h2 class="project-card-head">Portfolio Website</h2>
-                            <p className="project-details">
-                                This is my portfolio website made with React Js using react-dom, react-router
-                            </p>
-                        </div>
-                    </div>
-                    <div className="project-card">
-                        <div className="img-container">
-                        <img src={portfolioImg} alt="" className="project-card-img" />
-                        </div>
-                        <div className="description-container">
-                            <h2 class="project-card-head">Portfolio Website</h2>
-                            <p className="project-details">
-                                This is my portfolio website made with React Js using react-dom, react-router
-                            
-                            </p>
-                        </div>
-                    </div>
-                    <div className="project-card">
-                        <div className="img-container">
-                        <img src={portfolioImg} alt="" className="project-card-img" />
-                        </div>
-                        <div className="description-container">
-                            <h2 class="project-card-head">Portfolio Website</h2>
-                            <p className="project-details">
-                                This is my portfolio website made with React Js using react-dom, react-router
-                            </p>
-                        </div>
-                    </div>
-                   
-                  
-                </div>
-
+  state = {};
+  render() {
+    return (
+      <div className="project-container">
+        <div className="project-header-container">
+          <h3 className="project-head">My Projects in Shorts</h3>
+        </div>
+        <div className="project-cards-container">
+          <div className="project-card">
+            <div className="img-container">
+              <img src={portfolioImg} alt="" className="project-card-img" />
             </div>
-         );
-    }
+            <div className="description-container">
+              <h2 class="project-card-head">Portfolio Website</h2>
+              <p className="project-details">
+                This is my portfolio website made with React Js using react-dom,
+                react-router this is something that i have to solve
+              </p>
+            </div>
+          </div>
+          <div className="project-card">
+            <div className="img-container">
+              <img
+                src={covid19india}
+                alt=""
+                className="project-card-img"
+                id="covidImg"
+              />
+            </div>
+            <div className="description-container">
+              <h2 class="project-card-head">Covid19 India Tracker</h2>
+              <p className="project-details">
+                This is my covid19 tracker project made with react-js . In this
+                particular project I used API to get realtime data for indian
+                states only. In Future, there may be feature of getting every
+                country report on my website.
+              </p>
+            </div>
+          </div>
+          <div className="project-card">
+            <div className="img-container">
+              <img src={rejango} alt="" className="project-card-img" />
+            </div>
+            <div className="description-container">
+              <h2 class="project-card-head">Rejango Quoter</h2>
+              <p className="project-details">
+                In this Rejango Quoter project I used React-js for the frontend
+                and Django in the backend. In this project i worked on model
+                serializers and creating API in Django and from react fetching
+                those data.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Projects;
